f21ec7a42ae15410c9fc61f89582b140a0e191eb
Ben Grynhaus <bengr@microsoft.com>
2019-04-11 15:54:36 +0300

    Updates

---
+++ precache-manifest.055a3a4eedb4125b1c66684ca6ea5e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e27d30949dd16455cd5",
+    "url": "/fabric-nearest-color-finder/static/css/main.28ffe46f.chunk.css"
+  },
+  {
+    "revision": "7e27d30949dd16455cd5",
+    "url": "/fabric-nearest-color-finder/static/js/main.4400b477.chunk.js"
+  },
+  {
+    "revision": "f33c47b828213d09f696",
+    "url": "/fabric-nearest-color-finder/static/js/runtime~main.9b3ea5da.js"
+  },
+  {
+    "revision": "715b769c30d2991df35e",
+    "url": "/fabric-nearest-color-finder/static/js/2.170cb49e.chunk.js"
+  },
+  {
+    "revision": "98047b86785c00fb4c5f30b9f2945d8a",
+    "url": "/fabric-nearest-color-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fabric-nearest-color-finder/precache-manifest.055a3a4eedb4125b1c66684ca6ea5e3e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fabric-nearest-color-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
