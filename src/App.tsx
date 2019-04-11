bed112a2f053f248b622f28bb03414bfd00e435d
Ben Grynhaus <bengr@microsoft.com>
2019-04-11 12:54:49 +0300

    Remove unused styles & service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './App.css';
-
 import { FabricPalette } from './palettes/fabric';
 import { FluentPalette } from './palettes/fluent';
 import { IDisplayPalette } from './palettes/IDisplayPalette';
